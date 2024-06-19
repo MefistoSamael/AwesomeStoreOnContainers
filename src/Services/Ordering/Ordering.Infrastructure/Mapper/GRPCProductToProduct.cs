4893a4ff5c50efebdc479d0670434d9962c77790
Vassiliy Bychko <vasa060504@mail.ru>
2024-06-19 16:07:18 +0300

    Implemented GRPC

---
+++ src/Services/Ordering/Ordering.Infrastructure/Mapper/GRPCProductToProduct.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using grpcProductResponse = Ordering.Infrastructure.Protos.ProductResponse;
+using ProductResponse = Ordering.Application.Common.Models.ProductResponse;
+
+namespace Ordering.Infrastructure.Mapper;
+
+public class ProductToProductResponseProfile : Profile
+{
+    public ProductToProductResponseProfile()
+    {
+        CreateMap<grpcProductResponse, ProductResponse>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Id))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+            .ForMember(dest => dest.ImageUri, opt => opt.MapFrom(src => src.ImageUri))
+            .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price))
+            .ForMember(dest => dest.Categoties, opt => opt.MapFrom(src => src.Categories));
+    }
+}
--- src/Services/Ordering/Ordering.Infrastructure/Services/gRPCProductService.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Services/gRPCProductService.cs
@@ -1,13 +1,36 @@
-﻿using Ordering.Application.Services;
+﻿using AutoMapper;
+using Grpc.Net.Client;
+using Microsoft.Extensions.Options;
+using Ordering.Application.Common.Models;
+using Ordering.Application.Services;
+using Ordering.Infrastructure.Options;
+using Ordering.Infrastructure.Protos;
 using ProductResponse = Ordering.Application.Common.Models.ProductResponse;
 
 namespace Ordering.Infrastructure.Services;
 
 public class GRPCProductService : IProductService
 {
+    private readonly string _grpcServerAddres;
+    private readonly IMapper _mapper;
+
+    public GRPCProductService(IMapper mapper, IOptions<GrpcConnectionOptions> options)
+    {
+        _grpcServerAddres = options.Value.ConnectionString;
+        _mapper = mapper;
+    }
+
     public async Task<ProductResponse?> GetProductByIdAsync(string productId)
     {
-        return new ProductResponse
-        { Id = productId, Name = "Product", ImageUri = "https://image.com", Price = 100, Categoties = "Fence, wood" };
+        using var channel = GrpcChannel.ForAddress(_grpcServerAddres);
+
+        var client = new ProductGRPC.ProductGRPCClient(channel);
+
+        var request = new Request { ProductId = productId };
+
+        var grpcResponse = await client.ProdGetAsync(request);
+
+        var response = _mapper.Map<ProductResponse>(grpcResponse);
+        return response;
     }
 }
