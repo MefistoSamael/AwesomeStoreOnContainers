28a194d3e47b6f8ab6b3d690b123fddf953d10a3
Vassiliy Bychko <vasa060504@mail.ru>
2024-06-25 14:29:05 +0300

    GRPC Done

---
+++ src/Services/Catalog/Catalog.Infrastructure/Services/GRPCProductService.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Catalog.Domain.Abstractions;
+using Catalog.Infrastructure.Protos;
+using Grpc.Core;
+
+namespace Catalog.Infrastructure.Services;
+
+public class GRPCProductService : GRPCProduct.GRPCProductBase
+{
+    private readonly IProductRepostitory _productRepostitory;
+    private readonly IMapper _mapper;
+
+    public GRPCProductService(IProductRepostitory productRepostitory, IMapper mapper)
+    {
+        _productRepostitory = productRepostitory;
+        _mapper = mapper;
+    }
+
+    public override async Task<ProductResponse?> GetProductById(Request request, ServerCallContext context)
+    {
+        var product = await _productRepostitory.GetByIdAsync(request.ProductId, default);
+
+        if (product is null)
+        {
+            return null;
+        }
+
+        return _mapper.Map<ProductResponse>(product);
+    }
+}
